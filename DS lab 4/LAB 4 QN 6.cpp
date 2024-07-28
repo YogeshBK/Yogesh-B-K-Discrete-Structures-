c26317b92ba7b1d68620f662b4fedbf960dd5d44
YogeshBK <163974727+YogeshBK@users.noreply.github.com>
2024-07-28 15:35:51 +0545

    Add files via upload

---
+++ DS lab 4/LAB 4 QN 6.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int size, int target) {
+    int left = 0, right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; // Return the index of the target
+        }
+        if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1; // Return -1 if the target is not found
+}
+
+int main() {
+    int n, target, result;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the number to search: ");
+    scanf("%d", &target);
+
+    result = binarySearch(arr, n, target);
+
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found in the array\n");
+    }
+
+    return 0;
+}
+
